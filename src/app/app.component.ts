5c8d645b52c03b3d6638e6286a1468000c12d860
Chris Farmer <45003569+ign3u5@users.noreply.github.com>
2021-09-15 22:07:39 +0100

    Build working correctly

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { readFileSync } from 'fs';
 
 @Component({
   selector: 'app-root',
@@ -8,5 +7,4 @@ import { readFileSync } from 'fs';
 })
 export class AppComponent {
   title = 'blog';
-  md = readFileSync('.pages/2020-09-30-setting-up-a-blog-with-github-pages-and-jekyll.md', 'utf-8');
 }
